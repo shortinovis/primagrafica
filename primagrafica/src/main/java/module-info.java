b13b810935309b6e1c6b3ba8aa844932a0d0f805
Cortinovis <160122402+shortinovis@users.noreply.github.com>
2024-05-31 13:07:24 +0200

    Delete primagrafica directory

--- primagrafica/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.primagrafica {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.primagrafica to javafx.fxml;
-    exports com.example.primagrafica;
-}
